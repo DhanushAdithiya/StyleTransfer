552903a04771eec684c9ec1006313a657baacb21
DhanushAdithiya <k.b.dhanush02@gmail.com>
2024-03-07 21:44:19 +0530

    added module to save gif + save images at interval

--- train.py
+++ train.py
@@ -1,46 +1,52 @@
 import torch
-from utils import load_image, convert_tensor, save_image
+from utils import load_image, convert_tensor, save_image, save_gif
 from model import get_style, load_model
 from tqdm import tqdm
 import config
 
+
 def gram_matix(tensor):
     g = None
-    _,d,h,w = tensor.size()
-    tensor = tensor.view(d, h*w)
+    _, d, h, w = tensor.size()
+    tensor = tensor.view(d, h * w)
     g = torch.mm(tensor, tensor.t())
     return g
 
 
 vgg = load_model()
 
-content_img = load_image("content2.png")
-style_img = load_image("style_img.png")
+content_img = load_image("./data/content.png")
+style_img = load_image("./data/style.png")
 
-content_features = get_style(content_img,vgg)
-style_features = get_style(style_img,vgg)
+
+content_features = get_style(content_img, vgg)
+style_features = get_style(style_img, vgg)
 style_grams = {layer: gram_matix(style_features[layer]) for layer in style_features}
 
 
-target_img = load_image("content2.png").requires_grad_(True)
+target_img = load_image("./data/content.png").requires_grad_(True)
 
 
 optimizer = torch.optim.Adam([target_img], lr=0.003)
-epoch = 5000
+epoch = 2500
+img_off = config.IMG_OFF
 
 for i in tqdm(range(1, epoch + 1)):
     target_features = get_style(target_img, vgg)
-    content_loss = torch.mean((content_features["conv4_2"] - content_features["conv4_2"])**2)
-
+    content_loss = torch.mean(
+        (content_features["conv4_2"] - content_features["conv4_2"]) ** 2
+    )
 
     style_loss = 0
 
     for layer in config.STYLE_WEIGHTS:
         target_feature = target_features[layer]
-        _, d,h,w = target_feature.shape
+        _, d, h, w = target_feature.shape
         target_gram = gram_matix(target_feature)
         style_gram = style_grams[layer]
-        layer_style_loss = config.STYLE_WEIGHTS[layer] * torch.mean((target_gram - style_gram) ** 2)
+        layer_style_loss = config.STYLE_WEIGHTS[layer] * torch.mean(
+            (target_gram - style_gram) ** 2
+        )
 
         style_loss += layer_style_loss / (d * h * w)
 
@@ -49,6 +55,10 @@ for i in tqdm(range(1, epoch + 1)):
     total_loss.backward()
     optimizer.step()
 
-
     img_tens = convert_tensor(target_img)
-    save_image(img_tens, f"./generated/epoch_{i}.png")
+    if i % img_off == 0:
+        save_image(img_tens, f"./generated/epoch_{i}.png")
+
+
+    if i % epoch == 0:
+        save_gif("./generated/","testgif1")
--- utils.py
+++ utils.py
@@ -1,34 +1,49 @@
+import contextlib
 from PIL import Image
 from torchvision import transforms
 import numpy as np
+import glob
 
-def load_image(img_path, max_size=256):
+
+def load_image(img_path, max_size=400):
     img = Image.open(img_path).convert("RGB")
 
     size = img.size[0]
     if size > max_size:
         size = max_size
-    
-    transformation = transforms.Compose([
-        transforms.Resize(size),
-        transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-    ])
-    
+
+    transformation = transforms.Compose(
+        [
+            transforms.Resize(size),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        ]
+    )
+
     image = transformation(img)[:3, :, :].unsqueeze(0)
-    
-    return image    
+
+    return image
 
 
 def convert_tensor(tensor):
     image = tensor.to("cpu").clone().detach()
     image = image.numpy().squeeze()
-    image = image.transpose(1,2,0)
-    image = image * np.array((0.5,0.5,0.5)) + np.array((0.5,0.5,0.5))
-    image = image * 255  
+    image = image.transpose(1, 2, 0)
+    image = image * np.array((0.5, 0.5, 0.5)) + np.array((0.5, 0.5, 0.5))
+    image = image * 255
     image = image.astype(np.uint8)
     return image
+
 
 def save_image(arr, fp):
     image = Image.fromarray(arr)
     image.save(fp)
+
+def save_gif(fp,fn):
+    with contextlib.ExitStack() as stack:
+        imgs = (stack.enter_context(Image.open(f))
+                for f in sorted(glob.glob(f"./{fp}/*.png")))
+        img = next(imgs)
+
+        img.save(f"./{fn}.gif", format="GIF", append_images = imgs, save_all=True,duration=101, loop=0)
+                
